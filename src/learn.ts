50db22225a81d8ef50cd89b54ea15efa248fae70
Newton Ombese <newtonombese1@gmail.com>
2023-01-08 23:02:24 +0300

    working on the udagram project

---
+++ src/learn.ts
@@ -0,0 +1,15 @@
+const cost: number = 2
+console.log(cost)
+
+
+//almost same as struct in c++
+interface Person {
+    name: string
+    age: number
+}
+
+const james: Person = {
+    name: 'adrian',
+    age: 22
+}
+console.log(james.name +' '+ james.age)
