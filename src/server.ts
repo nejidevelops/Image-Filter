23e1babfde21df95d3d91a733bbb580f80c7a6fb
Newton Ombese <newtonombese1@gmail.com>
2023-01-09 08:53:40 +0300

    getting harder but will gradually manage a way out

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
-import express from 'express';
+import express, {Router, Request, Response} from 'express';
 import bodyParser from 'body-parser';
-import {Router, Request, Response} from 'express';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
 (async () => {
@@ -31,18 +30,15 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   app.get('/filteredimage', async (req: Request, res: Response) => {
-    const image_url = req.query.image_url.toString();
+    let { image_url } : {image_url: string} = req.query;
     if (!image_url) {
-      res.status(400).send('Image url is required');
+      res.status(400).send("The Image URL is Required :)");
     }
 
     const filtered_image = await filterImageFromURL(image_url);
-
     res.status(200).sendFile(filtered_image, () => {
       deleteLocalFiles([filtered_image]);
-    });
-
-    
+    })
   });
 
   //! END @TODO1
